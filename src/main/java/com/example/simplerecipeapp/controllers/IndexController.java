dbc158d9bfdd8cf802ef253198d5b9cf4cf18f26
Amila1610 <amilahasovic@hotmail.com>
2022-07-29 09:38:09 +0200

    Many to many JPA Relationships

--- src/main/java/com/example/simplerecipeapp/controllers/IndexController.java
+++ src/main/java/com/example/simplerecipeapp/controllers/IndexController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
-    @RequestMapping({"","/","index"})
-    public String getIndexPage(){
+    @RequestMapping({"", "/", "/index"})
+        public String getIndexPage(){
         System.out.println ("....Some message to say .......");
         return "index";
     }
